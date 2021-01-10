a682ed31d8ec232a64df6cad194d12d961cc0ebf
HawraAmeer <hwra.ameer97@gmail.com>
2021-01-10 15:28:36 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
 // Define the numbers and the operation here
+const num1 = 3;
+const num2 = 7;
+
+const userInput = prompt("What operation you want to do? ");
+// console.log(userInput);
 
 // Write if-statements here
+if (userInput === "+") {
+  console.log(`${num1} ${userInput} ${num2} = ${num1 + num2}`);
+} else if (userInput === "-") {
+  console.log(`${num1} ${userInput} ${num2} = ${num1 - num2}`);
+} else if (userInput === "*") {
+  console.log(`${num1} ${userInput} ${num2} = ${num1 * num2}`);
+} else if (userInput === "/") {
+  console.log(`${num1} ${userInput} ${num2} = ${num1 / num2}`);
+} else {
+  console.log("invalid operation!");
+}
